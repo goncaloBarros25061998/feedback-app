22936da5127dcf507ab7de356ef62b291b076cdc
Gonzalo Barros <goncalo.santiago.barros@outlook.com>
2023-11-02 11:42:18 +0000

    Animation for delete a feeback .

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -6,16 +6,25 @@ function FeedbackList({ feedback , handleDelete}) {
     if(!feedback || feedback.length === 0){
         return <p> No Feedback yet </p>
     }
-    return( 
+     return ( 
+        // the div class bellow shows how to animate when you delete a feedback
         <div className="feedback-list"> 
-        
+        <AnimatePresence>
         {feedback.map((item) => (
-            <FeedbackItem key={item.id} item={item}
-            handleDelete={handleDelete}/>
-        
+            <motion.div 
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+            >
+             <FeedbackItem key={item.id} item={item}
+             handleDelete={handleDelete}/>
+        </motion.div>
         ))}
+        </AnimatePresence>
          </div>
     )
+    
   /*   return( 
         <div className="feedback-list"> 
         {feedback.map((item) => (
