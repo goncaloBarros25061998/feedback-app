c202e6920c7fa98da030cb173b2f0c29e28b4f01
Gonzalo Barros <goncalo.santiago.barros@outlook.com>
2023-11-02 15:18:40 +0000

    Creation of Routes for the about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {v4 as uuidv4} from 'uuid'
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
 import { useState } from "react"
 import FeedbackData from "./data/FeedbackData"
-import Card from "./components/shared/Card"
+import AboutPage from './pages/AboutPage'
 function App() {
     
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -22,17 +23,24 @@ function App() {
     }
     // All the variables from the Feedback list, forms etc will be shown in bellow.
     return(
-        <>
-        
+     <Router>
         <Header  />
         <div className="container">
-            <FeedbackForm  handleAdd={addFeedback}/>
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            <Routes> 
+             <Route  exact path='/' element={
+                <>
+                <FeedbackForm  handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                </>
+             }>
+        
+             </Route>
 
-              
+             <Route path='/about' element={<AboutPage />} />  
+            </Routes>   
         </div>
-        </>
+     </Router>
     )
 }
 
